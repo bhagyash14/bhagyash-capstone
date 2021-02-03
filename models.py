48bcbe0f99c881298f997a065c3584c154c87677
bhagyash14 <72669538+bhagyash14@users.noreply.github.com>
2021-02-04 01:14:44 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,22 +6,27 @@ import json
 
 database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = \
+    "sqlite:///{}".format(os.path.join(project_dir, database_filename))
 
 db = SQLAlchemy()
 
+
 def setup_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:mamali14@@localhost:5432/capstone'
+    app.config["SQLALCHEMY_DATABASE_URI"] =\
+        'postgresql://postgres:mamali14@@localhost:5432/capstone'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     db.app = app
     db.init_app(app)
     migrate = Migrate(app, db)
 
+
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
 
-class Actor(db.Model):  
+
+class Actor(db.Model):
     __tablename__ = 'actors'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -33,38 +38,43 @@ class Actor(db.Model):
         self.name = name
         self.gender = gender
         self.age = age
+
     def insert(self):
         db.session.add(self)
         db.session.commit()
+
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
     def update(self):
         db.session.commit()
+
     def format(self):
         return {
             'id': self.id,
-            'name' : self.name,
+            'name': self.name,
             'gender': self.gender,
             'age': self.age
         }
 
-class Movie(db.Model):  
+
+class Movie(db.Model):
     __tablename__ = 'movies'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     release_date = db.Column(db.Date)
-    #actors = db.relationship('Actor', backref='movies')
+#    #actors = db.relationship('Actor', backref='movies')
 
-    def __init__(self, title, release_date) :
+    def __init__(self, title, release_date):
         self.title = title
         self.release_date = release_date
 
     def insert(self):
         db.session.add(self)
         db.session.commit()
-  
+
     def update(self):
         db.session.commit()
 
@@ -75,6 +85,6 @@ class Movie(db.Model):
     def format(self):
         return {
             'id': self.id,
-            'title' : self.title,
+            'title': self.title,
             'release_date': self.release_date
         }
