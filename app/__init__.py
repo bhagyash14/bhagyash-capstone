c65c322215564e36b8401aa75d8cb69fd3170b01
Bhagyashree Sahoo <bhagysah@amdocs.com>
2021-02-03 15:09:07 +0530

    make it better

--- app/__init__.py
+++ app/__init__.py
@@ -200,3 +200,8 @@ def create_app(test_config=None):
         return response
 
     return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
