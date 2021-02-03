aa3d2b7d42d1ea7a3cdcbb031fcf77cf293df034
bhagyash14 <72669538+bhagyash14@users.noreply.github.com>
2021-02-04 01:58:05 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,3 +1,4 @@
+import os
 import json
 from flask import request, _request_ctx_stack
 from functools import wraps
@@ -6,7 +7,7 @@ from urllib.request import urlopen
 
 
 AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
-ALGORITHMS = os.environ.get['ALGORITHMS']
+ALGORITHMS = os.environ.get('ALGORITHMS')
 API_AUDIENCE = os.environ.get('API_AUDIENCE')
 CLIENT_ID = os.environ.get('CLIENT_ID')
 
